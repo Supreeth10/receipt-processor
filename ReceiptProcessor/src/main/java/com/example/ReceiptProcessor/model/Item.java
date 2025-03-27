59889ae84e17c5cb696cf34807d8bc727b75e785
supreeth10 <smuddu2@pdx.edu>
2025-03-26 23:58:34 -0700

    Added files and code for handling errors and exceptions

--- ReceiptProcessor/src/main/java/com/example/ReceiptProcessor/model/Item.java
+++ ReceiptProcessor/src/main/java/com/example/ReceiptProcessor/model/Item.java
@@ -1,9 +1,19 @@
 package com.example.ReceiptProcessor.model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class Item {
+    @NotBlank(message = "Short description is required.")
     private String shortDescription;
+
+    @NotBlank(message = "Price is required.")
+    @Pattern(regexp = "^\\d+\\.\\d{2}$", message = "Price must be a valid decimal amount (e.g., 2.25).")
     private String price;
 }
